b035cbfae14a8fd9c5a382c9325065e6f80a7ead
od <od@od>
2014-08-01 11:05:38 -0500

    funciona

--- src/java/Controller/DataView.java
+++ src/java/Controller/DataView.java
@@ -12,81 +12,74 @@ import javax.faces.model.SelectItem;
 import javax.faces.model.SelectItemGroup;
 import org.primefaces.model.DualListModel;
 import beans.connection.ConnectionJdbcMB;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.context.FacesContext;
 import managedBeans.login.ApplicationControlMB;
-
+import org.primefaces.event.SlideEndEvent;
 
 @ManagedBean
 @SessionScoped
 public class DataView {
 
     private DualListModel<SelectItem> variables;
-    private String fact;  
+    private String fact;
     private List<SelectItem> facts;
-    
-    private List<SelectItem> variablesSource;    
-    private List<SelectItem> variablesTarget;  
-    
+    private List<SelectItem> variablesSource;
+    private List<SelectItem> variablesTarget;
     ConnectionJdbcMB connectionJdbcMB;
     ApplicationControlMB applicationControlMB;
     FacesContext context;
-    
     private SelectItem selectedList;
-    
     private int countColNameData;
     private int countData;
     private ArrayList<Object[]> data;
     private ArrayList<String> colNameData;
-    
-    
-     public DataView() {
+
+    public DataView() {
     }
-    
+
     public DataView(int countData, int countColNameData, ArrayList<Object[]> data, ArrayList<String> colNameData) {
         this.countData = countData;
-        this.countColNameData = countColNameData;        
+        this.countColNameData = countColNameData;
         this.data = data;
         this.colNameData = colNameData;
     }
-    
-    
+
     @PostConstruct
     public void init() {
         variablesSource = new ArrayList<SelectItem>();
-        variablesTarget = new ArrayList<SelectItem>(); 
-        
+        variablesTarget = new ArrayList<SelectItem>();
+
         SelectItemGroup g1 = new SelectItemGroup("Fatales");
-        g1.setSelectItems(new SelectItem[] {new SelectItem("fact_murder", "Homicidios"), new SelectItem("fact_suicide", "Suicidios"),
-            new SelectItem("fact_traffic", "Muertes Accidentes Tránsito"), new SelectItem("fact_accidents", "Muertes Accidentales")});
-         
+        g1.setSelectItems(new SelectItem[]{new SelectItem("fact_murder", "Homicidios"), new SelectItem("fact_suicide", "Suicidios"),
+                    new SelectItem("fact_traffic", "Muertes Accidentes Tránsito"), new SelectItem("fact_accidents", "Muertes Accidentales")});
+
         SelectItemGroup g2 = new SelectItemGroup("No Fatales");
-        g2.setSelectItems(new SelectItem[] {new SelectItem("fact_interpersonal", "Interpersonal"), new SelectItem("fact_intrafamiliar", "Intrafamiliar"),
-            new SelectItem("fact_self_inflicted", "Auto Inflingido"), new SelectItem("fact_transport", "Lesiones de Tránsito"), 
-            new SelectItem("fact_unintencional", "No intencional")});
-         
+        g2.setSelectItems(new SelectItem[]{new SelectItem("fact_interpersonal", "Interpersonal"), new SelectItem("fact_intrafamiliar", "Intrafamiliar"),
+                    new SelectItem("fact_self_inflicted", "Auto Inflingido"), new SelectItem("fact_transport", "Lesiones de Tránsito"),
+                    new SelectItem("fact_unintencional", "No intencional")});
+
         facts = new ArrayList<SelectItem>();
         facts.add(g1);
-        facts.add(g2); 
+        facts.add(g2);
 
         variables = new DualListModel<SelectItem>(variablesSource, variablesTarget);
 
+
     }
-    
-   
+
     public void load() {
-        List<SelectItem> variablesSource = new ArrayList<SelectItem>();
-        List<SelectItem> variablesTarget = new ArrayList<SelectItem>();
-        
         context = FacesContext.getCurrentInstance();
         connectionJdbcMB = (ConnectionJdbcMB) context.getApplication().evaluateExpressionGet(context, "#{connectionJdbcMB}", ConnectionJdbcMB.class);
         connectionJdbcMB.connectToDb();
         ResultSet rs = connectionJdbcMB.consult("Select * from common_variables");
-        ResultSet rsOwn = connectionJdbcMB.consult("Select * from own_variables where fact like '"+this.fact+"'");
-        
-         try {
+        ResultSet rsOwn = connectionJdbcMB.consult("Select * from own_variables where fact like '" + this.fact + "'");
+
+        try {
             while (rs.next()) {
                 String descriptionVariable = rs.getString("description");
                 String nameVariable = rs.getString("name");
@@ -104,26 +97,47 @@ public class DataView {
         } catch (SQLException ex) {
             Logger.getLogger(DataView.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
-             
-        variables = new DualListModel<SelectItem>(variablesSource, variablesTarget);
+
+
+        variables = new DualListModel<>(variablesSource, variablesTarget);
+
+    }
+
+    public void loadData() throws SQLException {
+        colNameData = new ArrayList<>();
+        data = new ArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<String>(); 
+        
+        for (int i = 0; i < this.variables.getTarget().size(); i++) {
+            for (SelectItem fuente : variablesSource) {                
+                String a=(String)fuente.getValue();
+                String b=String.valueOf(variables.getTarget().get(i));
+                
+                if (a.equals(b)) {
+                    colNameData.add(fuente.getLabel());
+                    break;
+                }
+            }            
+        }
+        
+        ResultSet rs = connectionJdbcMB.consult("Select * from common_variables");
+        
+        while (rs.next()){
+            int i = 1;
+            while(i<= 3){
+                arrayList.add(rs.getString(i++));
+            }
+        }
+        System.out.println(""+arrayList);
+        
+        data.add(new Object[]{arrayList});
+        //data.add(new Object[]{"Omar","sdd"});
+      
+        
         
     }
     
-    public void loadData() {
-        colNameData = new ArrayList<String>();
-        data = new ArrayList<Object[]>();
-        
-        System.out.println("event"+this.fact);
-        System.out.println("vari"+this.variables.getTarget());
-        
-        
-             
-        colNameData.add("Columna1");
-        colNameData.add("Columna2");
-        
-    }
-
+    
     public DualListModel<SelectItem> getVariables() {
         return variables;
     }
@@ -131,15 +145,15 @@ public class DataView {
     public void setVariables(DualListModel<SelectItem> variables) {
         this.variables = variables;
     }
-    
+
     public String getFact() {
-        return fact;        
+        return fact;
     }
- 
+
     public void setFact(String fact) {
         this.fact = fact;
     }
-   
+
     public List<SelectItem> getFacts() {
         return facts;
     }
@@ -147,7 +161,7 @@ public class DataView {
     public SelectItem getSelectedList() {
         return this.selectedList;
     }
-    
+
     public int getCountColNameData() {
         return countColNameData;
     }
@@ -178,5 +192,5 @@ public class DataView {
 
     public void setData(ArrayList<Object[]> data) {
         this.data = data;
-    }    
+    }
 }
